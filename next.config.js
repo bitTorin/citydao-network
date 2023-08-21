720936da26a371450b7ca77563f4aa43596bf449
bitTorin <torin.miner@gmail.com>
2023-08-20 19:19:41 -0500

    api/token updates

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://citydao-network.vercel.app, https://citydao-network-bittorin.vercel.app/, https://citydao-network-git-main-bittorin.vercel.app/" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
-
 }
 
 module.exports = {
-    nextConfig,
-    env: {
-        MAPBOX_TOKEN: process.env.MAPBOX,
-    },
+    nextConfig
 };
--- src/api/mapbox.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default async (req, res) => {
-  console.log(`${process.env.MAPBOX}`)
-  const URL = `https://api.i.require.keys/?&api_key=${process.env.MAPBOX}`;
-  const response = await axios.get(URL);
-  res.status(200).json({ data: response.data })
-}
